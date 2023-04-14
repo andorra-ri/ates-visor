c67463aeada43b0ec5b5e7a978838537f474538d
Marc Vilella <mvilella.and@gmail.com>
2023-04-14 23:09:36 +0200

    feat: Add localization

---
+++ src/i18n.ts
@@ -0,0 +1,19 @@
+import { createI18n } from 'vue-i18n';
+
+/* eslint-disable-next-line import/no-unresolved */
+import messages from '@intlify/unplugin-vue-i18n/messages';
+
+export const BROWSER_LOCALE = Intl.DateTimeFormat().resolvedOptions().locale.split('-')[0];
+
+const i18n = createI18n({
+  legacy: false,
+  locale: BROWSER_LOCALE,
+  fallbackLocale: 'en',
+  messages,
+});
+
+export default i18n;
+
+export const setLocale = (locale = BROWSER_LOCALE) => {
+  i18n.global.locale.value = locale;
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import vue from '@vitejs/plugin-vue';
-import vueI18n from '@intlify/vite-plugin-vue-i18n';
+import vueI18n from '@intlify/unplugin-vue-i18n/vite';
 import yaml from '@modyfi/vite-plugin-yaml';
 import autoprefixer from 'autoprefixer';
 
