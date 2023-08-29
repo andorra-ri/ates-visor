de8d21b303df42772d3e3ab7811774ccfe6ba0a2
Marc Vilella <mvilella.and@gmail.com>
2023-08-29 12:17:18 +0200

    feat: Add avalanche notification link

--- src/layouts/Toolbar.vue
+++ src/layouts/Toolbar.vue
@@ -3,11 +3,12 @@
     <RouteSelect
       :routes="store.state.routes"
       @select="store.selectRoute" />
+    <AvalancheNotify />
     <AvalancheRisk :risk="store.state.avalancheRisk" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { RouteSelect, AvalancheRisk } from './partials';
+import { RouteSelect, AvalancheRisk, AvalancheNotify } from './partials';
 import store from '/@/store';
 </script>
--- src/layouts/partials/index.ts
+++ src/layouts/partials/index.ts
@@ -1,2 +1,3 @@
 export { default as RouteSelect } from './RouteSelect.vue';
 export { default as AvalancheRisk } from './AvalancheRisk.vue';
+export { default as AvalancheNotify } from './AvalancheNotify.vue';
