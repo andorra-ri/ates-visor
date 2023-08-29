bdc8e91f5c344942a427d7c882a23524ed4cce61
Marc Vilella <mvilella.and@gmail.com>
2023-08-29 02:52:04 +0200

    feat: Add Modal component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Dropdown } from './Dropdown.vue';
 export { default as PickList } from './PickList.vue';
 export { default as Selector } from './Selector.vue';
+export { default as Modal } from './Modal.vue';
 export { default as SearchInput } from './SearchInput.vue';
 export { default as SortInput } from './SortInput.vue';
