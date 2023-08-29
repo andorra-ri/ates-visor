158ad82a0410078d17cba745465759e8b1c3b7fe
Marc Vilella <mvilella.and@gmail.com>
2023-08-29 12:16:33 +0200

    refactor: Set toolbar style as global

--- src/layouts/Toolbar.vue
+++ src/layouts/Toolbar.vue
@@ -11,17 +11,3 @@
 import { RouteSelect, AvalancheRisk } from './partials';
 import store from '/@/store';
 </script>
-
-<style lang="scss" scoped>
-.toolbar {
-  @extend %container-strong;
-
-  position: fixed;
-  margin: 0.75rem;
-  display: flex;
-  align-items: stretch;
-  z-index: 1;
-
-  & > * + * { border-left: 1px solid var(--color-border); }
-}
-</style>
