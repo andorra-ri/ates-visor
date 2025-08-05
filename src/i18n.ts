1fc54f98062ce0e05951f1c815991ca5a20e6ce6
Marc Jorge <mjorge@ari.ad>
2025-08-05 16:47:15 +0200

    chore(i18n): add helper function to force browser locale

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,6 +3,17 @@ import { createI18n } from 'vue-i18n';
 /* eslint-disable-next-line import/no-unresolved */
 import messages from '@intlify/unplugin-vue-i18n/messages';
 
+/**
+ * Helper to test different browser-locales (es-ES, en-GB)
+ ***** TODO: remove this in production *****
+* */
+// Object.defineProperty(Intl.DateTimeFormat.prototype, 'resolvedOptions', {
+//   value() {
+//     return { locale: 'en-US' };
+//   },
+// });
+/** ********* */
+
 export const BROWSER_LOCALE = Intl.DateTimeFormat().resolvedOptions().locale.split('-')[0];
 
 const i18n = createI18n({
